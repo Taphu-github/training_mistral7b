2e275fb2c6b3fb5dbd4d6dfac7d55128b4ce7d63
Taphu <99734455+Taphu-github@users.noreply.github.com>
2024-01-31 10:14:48 +0600

    Create inferencinguploadedmodelusinghuggingface.py
    first commit

---
+++ inferencinguploadedmodelusinghuggingface.py
@@ -0,0 +1,27 @@
+from peft import AutoPeftModelForCausalLM
+from transformers import GenerationConfig
+from transformers import AutoTokenizer
+import torch
+tokenizer = AutoTokenizer.from_pretrained("Taphu/chatbot_mistral7b")
+
+inputs = tokenizer("### Question: How many divisions are there in GovTech", return_tensors="pt").to("cuda")
+
+model = AutoPeftModelForCausalLM.from_pretrained(
+    "Taphu/chatbot_mistral7b",
+    low_cpu_mem_usage=True,
+    return_dict=True,
+    torch_dtype=torch.float16,
+    device_map="cuda")
+
+generation_config = GenerationConfig(
+    do_sample=True,
+    top_k=1,
+    temperature=0.1,
+    max_new_tokens=256,
+    pad_token_id=tokenizer.eos_token_id
+)
+import time
+st_time = time.time()
+outputs = model.generate(**inputs, generation_config=generation_config)
+print(tokenizer.decode(outputs[0], skip_special_tokens=True))
+print(time.time()-st_time)
