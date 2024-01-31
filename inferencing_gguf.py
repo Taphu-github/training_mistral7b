41046309e46f59ed412def4e8119a9005603daed
Taphu <99734455+Taphu-github@users.noreply.github.com>
2024-01-31 10:17:07 +0600

    Create inferencing_gguf.py
    first commit

---
+++ inferencing_gguf.py
@@ -0,0 +1,19 @@
+
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain_community.llms import LlamaCpp
+
+llm = LlamaCpp(
+    model_path="mistral7b_govtech_v1.gguf",
+    n_gpu_layers=1,
+    n_batch=512,
+    n_ctx=2048,
+    f16_kv=True,
+    callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
+    verbose=True,
+)
+
+llm("###Question: Questions i want to ask ?")
+
+llm.invoke("###Question: Questions i want to ask?")
+
